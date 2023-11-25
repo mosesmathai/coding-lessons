b1c31c97614538170d9cf223f7b50997a9bfd4ad
unknown <mosesmathai9@gmail.com>
2023-11-25 16:25:33 +0300

    content

--- app/page.js
+++ app/page.js
@@ -19,7 +19,7 @@ export default function Home() {
       <p className="indent-10 mb-4">
         We will be operating online using Zoom and enrolments will be at the beginning of every month. For those who prefer physical classes, we can be meeting in Juja. Always remember that to be good at any skill, the learning process should never end. By joining our program, we will teach you how to use: 
       </p>
-      <div className="flex justify-center text-green-500">
+      <div className="flex justify-center text-green-500 mb-6">
         <div>
           <span>&#8226; HTML - &#123;markup language&#125;</span> <br/>
           <span>&#8226; CSS  - &#123;styling language&#125;</span> <br/>
@@ -32,6 +32,31 @@ export default function Home() {
           <span>&#8226; How to create Websites</span> <br/>
           <span>&#8226; How to secure websites and applications</span> <br/>
         </div>
+      </div>
+      <div className="mb-4 flex justify-center underline decoration-green-500">
+        <h1 className="text-green-500 font-bold">E-Commerce Websites Available</h1>
+      </div>
+      <p className="indent-10 mb-4">
+        Our E-Commerce website has a lifetime warranty. The setup process takes a maximum of <span className="text-green-500"> 4 days</span>. The price for Kenyan Citizens is <span className="text-green-500"> 20,000 Ksh</span> while for foreigners it is <span className="text-green-500"> $200</span>. This amount will cover 2 custom domains valid for 1 year. After it expires, the client will have an option to either renew the custom domains or to use the free domains provided by our host vercel. Bellow are some of the features that this website has.
+      </p>
+      <div className="flex justify-center text-green-500 mb-6">
+        <div>
+          <span>&#8226; Supports M-Pesa transactions</span> <br/>
+          <span>&#8226; Supports Visa Cards transactions</span> <br/>
+          <span>&#8226; Supports Master Cards transactions</span> <br/>
+          <span>&#8226; You can add unlimited number of products</span> <br/>
+          <span>&#8226; The website has multiple Themes</span> <br/>
+          <span>&#8226; The website comes with its own Administation Panel</span> <br/>
+          <span>&#8226; There is a separate section for inquiries</span> <br/>
+          <span>&#8226; There is a separate section for updates</span> <br/>
+          <span>&#8226; You can filter products in the category section</span> <br/>
+        </div>
+      </div>
+      <p className="mb-4">
+        Click the button bellow to test the E-Commerce website.
+      </p>
+      <div className="flex justify-center">
+        <a className="no-underline bg-purple-900 py-2 px-4 rounded-md cursor-pointer" target="_blank" href="https://business-website-pied.vercel.app/">Sample</a>
       </div>
       <div className="mt-10">
         <span>Yours Sincerely,</span> <br/>
